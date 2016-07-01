53e8be35735923f89c5eb0a82dc3049df6451354
Ludo Ruisch <admin@ludoruisch.nl>
2016-07-01 19:08:33 +0200

    First commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.InjectTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.DelayTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.EndTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.pnl_searching = New System.Windows.Forms.Panel()
+        Me.pb_searching = New System.Windows.Forms.ProgressBar()
+        Me.lbl_searching = New System.Windows.Forms.Label()
+        Me.pnl_injected = New System.Windows.Forms.Panel()
+        Me.lbl_process_name = New System.Windows.Forms.Label()
+        Me.lbl_process_id = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lbl_injected = New System.Windows.Forms.Label()
+        Me.AnimateTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.pnl_injecting = New System.Windows.Forms.Panel()
+        Me.pb_injecting = New System.Windows.Forms.ProgressBar()
+        Me.lbl_injecting = New System.Windows.Forms.Label()
+        Me.pnl_searching.SuspendLayout()
+        Me.pnl_injected.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnl_injecting.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'InjectTimer
+        '
+        Me.InjectTimer.Interval = 5000
+        '
+        'DelayTimer
+        '
+        Me.DelayTimer.Interval = 10000
+        '
+        'EndTimer
+        '
+        Me.EndTimer.Interval = 5000
+        '
+        'pnl_searching
+        '
+        Me.pnl_searching.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnl_searching.Controls.Add(Me.pb_searching)
+        Me.pnl_searching.Controls.Add(Me.lbl_searching)
+        Me.pnl_searching.Location = New System.Drawing.Point(12, 224)
+        Me.pnl_searching.Name = "pnl_searching"
+        Me.pnl_searching.Size = New System.Drawing.Size(332, 100)
+        Me.pnl_searching.TabIndex = 0
+        '
+        'pb_searching
+        '
+        Me.pb_searching.Location = New System.Drawing.Point(177, 44)
+        Me.pb_searching.Maximum = 10
+        Me.pb_searching.Name = "pb_searching"
+        Me.pb_searching.Size = New System.Drawing.Size(119, 23)
+        Me.pb_searching.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.pb_searching.TabIndex = 0
+        '
+        'lbl_searching
+        '
+        Me.lbl_searching.AutoSize = True
+        Me.lbl_searching.Location = New System.Drawing.Point(174, 16)
+        Me.lbl_searching.Name = "lbl_searching"
+        Me.lbl_searching.Size = New System.Drawing.Size(122, 13)
+        Me.lbl_searching.TabIndex = 1
+        Me.lbl_searching.Text = "Searching for process ..."
+        '
+        'pnl_injected
+        '
+        Me.pnl_injected.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnl_injected.Controls.Add(Me.lbl_process_name)
+        Me.pnl_injected.Controls.Add(Me.lbl_process_id)
+        Me.pnl_injected.Controls.Add(Me.PictureBox1)
+        Me.pnl_injected.Controls.Add(Me.lbl_injected)
+        Me.pnl_injected.Location = New System.Drawing.Point(12, 118)
+        Me.pnl_injected.Name = "pnl_injected"
+        Me.pnl_injected.Size = New System.Drawing.Size(332, 100)
+        Me.pnl_injected.TabIndex = 1
+        '
+        'lbl_process_name
+        '
+        Me.lbl_process_name.AutoSize = True
+        Me.lbl_process_name.Location = New System.Drawing.Point(21, 26)
+        Me.lbl_process_name.Name = "lbl_process_name"
+        Me.lbl_process_name.Size = New System.Drawing.Size(74, 13)
+        Me.lbl_process_name.TabIndex = 4
+        Me.lbl_process_name.Text = "Processname:"
+        '
+        'lbl_process_id
+        '
+        Me.lbl_process_id.AutoSize = True
+        Me.lbl_process_id.Location = New System.Drawing.Point(21, 44)
+        Me.lbl_process_id.Name = "lbl_process_id"
+        Me.lbl_process_id.Size = New System.Drawing.Size(62, 13)
+        Me.lbl_process_id.TabIndex = 2
+        Me.lbl_process_id.Text = "Process ID:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Image = Global.Manager.My.Resources.Resources.icon_803718_640
+        Me.PictureBox1.Location = New System.Drawing.Point(232, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'lbl_injected
+        '
+        Me.lbl_injected.AutoSize = True
+        Me.lbl_injected.Location = New System.Drawing.Point(3, 3)
+        Me.lbl_injected.Name = "lbl_injected"
+        Me.lbl_injected.Size = New System.Drawing.Size(67, 13)
+        Me.lbl_injected.TabIndex = 0
+        Me.lbl_injected.Text = "DLL injected"
+        '
+        'AnimateTimer
+        '
+        Me.AnimateTimer.Enabled = True
+        '
+        'pnl_injecting
+        '
+        Me.pnl_injecting.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnl_injecting.Controls.Add(Me.pb_injecting)
+        Me.pnl_injecting.Controls.Add(Me.lbl_injecting)
+        Me.pnl_injecting.Location = New System.Drawing.Point(12, 12)
+        Me.pnl_injecting.Name = "pnl_injecting"
+        Me.pnl_injecting.Size = New System.Drawing.Size(332, 100)
+        Me.pnl_injecting.TabIndex = 2
+        '
+        'pb_injecting
+        '
+        Me.pb_injecting.Location = New System.Drawing.Point(177, 44)
+        Me.pb_injecting.Maximum = 10
+        Me.pb_injecting.Name = "pb_injecting"
+        Me.pb_injecting.Size = New System.Drawing.Size(119, 23)
+        Me.pb_injecting.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.pb_injecting.TabIndex = 0
+        '
+        'lbl_injecting
+        '
+        Me.lbl_injecting.AutoSize = True
+        Me.lbl_injecting.Location = New System.Drawing.Point(174, 16)
+        Me.lbl_injecting.Name = "lbl_injecting"
+        Me.lbl_injecting.Size = New System.Drawing.Size(85, 13)
+        Me.lbl_injecting.TabIndex = 1
+        Me.lbl_injecting.Text = " Injecting DLL ..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(356, 122)
+        Me.Controls.Add(Me.pnl_searching)
+        Me.Controls.Add(Me.pnl_injecting)
+        Me.Controls.Add(Me.pnl_injected)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Vermintide DLL Injector"
+        Me.pnl_searching.ResumeLayout(False)
+        Me.pnl_searching.PerformLayout()
+        Me.pnl_injected.ResumeLayout(False)
+        Me.pnl_injected.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnl_injecting.ResumeLayout(False)
+        Me.pnl_injecting.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents InjectTimer As System.Windows.Forms.Timer
+    Friend WithEvents DelayTimer As System.Windows.Forms.Timer
+    Friend WithEvents EndTimer As System.Windows.Forms.Timer
+    Friend WithEvents pnl_searching As System.Windows.Forms.Panel
+    Friend WithEvents lbl_searching As System.Windows.Forms.Label
+    Friend WithEvents pb_searching As System.Windows.Forms.ProgressBar
+    Friend WithEvents pnl_injected As System.Windows.Forms.Panel
+    Friend WithEvents lbl_injected As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lbl_process_id As System.Windows.Forms.Label
+    Friend WithEvents AnimateTimer As System.Windows.Forms.Timer
+    Friend WithEvents lbl_process_name As System.Windows.Forms.Label
+    Friend WithEvents pnl_injecting As System.Windows.Forms.Panel
+    Friend WithEvents pb_injecting As System.Windows.Forms.ProgressBar
+    Friend WithEvents lbl_injecting As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,215 @@
+﻿Imports System.Threading
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Public Const PROCESS_VM_READ = &H10
+    Public Const TH32CS_SNAPPROCESS = &H2
+    Public Const MEM_COMMIT = 4096
+    Public Const PAGE_READWRITE = 4
+    Public Const PROCESS_CREATE_THREAD = (&H2)
+    Public Const PROCESS_VM_OPERATION = (&H8)
+    Public Const PROCESS_VM_WRITE = (&H20)
+
+    Public Declare Function ReadProcessMemory Lib "kernel32" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer As String, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Integer
+    Public Declare Function LoadLibrary Lib "kernel32" Alias "LoadLibraryA" (ByVal lpLibFileName As String) As Integer
+    Public Declare Function VirtualAllocEx Lib "kernel32" (ByVal hProcess As Integer, ByVal lpAddress As Integer, ByVal dwSize As Integer, ByVal flAllocationType As Integer, ByVal flProtect As Integer) As Integer
+    Public Declare Function WriteProcessMemory Lib "kernel32" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer As String, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Integer
+    Public Declare Function GetProcAddress Lib "kernel32" (ByVal hModule As Integer, ByVal lpProcName As String) As Integer
+    Private Declare Function GetModuleHandle Lib "Kernel32" Alias "GetModuleHandleA" (ByVal lpModuleName As String) As Integer
+    Public Declare Function CreateRemoteThread Lib "kernel32" (ByVal hProcess As Integer, ByVal lpThreadAttributes As Integer, ByVal dwStackSize As Integer, ByVal lpStartAddress As Integer, ByVal lpParameter As Integer, ByVal dwCreationFlags As Integer, ByRef lpThreadId As IntPtr) As Integer
+    Public Declare Function OpenProcess Lib "kernel32" (ByVal dwDesiredAccess As Integer, ByVal bInheritHandle As Integer, ByVal dwProcessId As Integer) As Integer
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    Private Declare Function CloseHandle Lib "kernel32" Alias "CloseHandleA" (ByVal hObject As Integer) As Integer
+
+    'Dim LauncherPath As String = "F:\SteamLibrary\steamapps\common\Warhammer End Times Vermintide\launcher\launcher.exe"
+    'Dim VermintidePath As String = "F:\SteamLibrary\steamapps\common\Warhammer End Times Vermintide\binaries\vermintide.exe"
+    'Dim VermintideDLL As String = "C:\Users\Dude\Desktop\Vermintide\vermintide_new.dll"
+    Dim DLLPath As String
+    Dim VermintideName As String = "vermintide"
+    Dim VermintideProcess As Process
+
+    Private Sub Inject()
+        Try
+            'Inject Dll
+            Dim lpThreadId As IntPtr
+            Dim TargetProcessHandle As Integer = OpenProcess(&H1F0FFF, 0, VermintideProcess.Id)
+            Dim pszLibFileRemote As String = (DLLPath)
+            Dim pfnStartAddr As Integer = GetProcAddress(GetModuleHandle("Kernel32"), "LoadLibraryA")
+            Dim TargetBufferSize As Integer = 1 + Len(pszLibFileRemote)
+            Dim Rtn As Integer
+            Dim LoadLibParamAdr As Integer
+            LoadLibParamAdr = VirtualAllocEx(TargetProcessHandle, 0, TargetBufferSize, MEM_COMMIT, PAGE_READWRITE)
+            Rtn = WriteProcessMemory(TargetProcessHandle, LoadLibParamAdr, pszLibFileRemote, TargetBufferSize, 0)
+            CreateRemoteThread(TargetProcessHandle, 0, 0, pfnStartAddr, LoadLibParamAdr, 0, lpThreadId)            
+            ' Panels
+            'pnl_injecting.Visible = False
+            'pnl_injected.Visible = True
+            SwitchPanel(Panel.Injected)
+            Update_Injected(VermintideProcess)            
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.OkOnly, "Error")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InjectTimer.Start()
+        DLLPath = Application.StartupPath + "\vermintide_new.dll"
+Browse:
+        If Not My.Computer.FileSystem.FileExists(DLLPath) Then
+            Dim FindDLL As New OpenFileDialog
+            FindDLL.Title = "Open DLL to inject"
+            FindDLL.Filter = "(*.dll)|*.dll"
+            If FindDLL.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                DLLPath = FindDLL.FileName
+            End If
+        End If
+        If Not My.Computer.FileSystem.FileExists(DLLPath) Then
+            If MsgBox("DLL not found", MsgBoxStyle.RetryCancel, "DLL not found") = MsgBoxResult.Retry Then
+                GoTo Browse
+            Else
+                Application.Exit()
+            End If
+        End If
+    End Sub
+
+    Private Sub InjectTimer_Tick(sender As Object, e As EventArgs) Handles InjectTimer.Tick
+        Dim check_process As Process() = Process.GetProcessesByName(VermintideName)
+        If check_process.Length > 0 Then
+            VermintideProcess = check_process(0)
+            ' Timers
+            InjectTimer.Stop()
+            DelayTimer.Start()
+            EndTimer.Start()
+            ' Panels
+            SwitchPanel(Panel.Injecting)
+        End If
+    End Sub
+
+    Private Sub DelayTimer_Tick(sender As Object, e As EventArgs) Handles DelayTimer.Tick
+        If Not IsNothing(VermintideProcess) Then
+            DelayTimer.Stop()
+            Inject()
+        End If
+    End Sub
+
+    Private Sub EndTimer_Tick(sender As Object, e As EventArgs) Handles EndTimer.Tick
+        If VermintideProcess.HasExited Then
+            VermintideProcess = Nothing
+            InjectTimer.Start()
+            EndTimer.Stop()
+            SwitchPanel(Panel.Searching)
+        End If
+    End Sub
+
+    Private Sub AnimateTimer_Tick(sender As Object, e As EventArgs) Handles AnimateTimer.Tick
+        If pnl_searching.Visible Then
+            If pb_searching.Value < pb_searching.Maximum Then
+                pb_searching.Value += 1
+            Else
+                pb_searching.Value = pb_searching.Minimum
+            End If
+        End If
+        If pnl_injecting.Visible Then
+            If pb_injecting.Value < pb_injecting.Maximum Then
+                pb_injecting.Value += 1
+            Else
+                pb_injecting.Value = pb_injecting.Minimum
+            End If
+        End If
+    End Sub
+
+    Private Sub pnl_searching_SizeChanged(sender As Object, e As EventArgs) Handles pnl_searching.SizeChanged
+        FitSearch()
+    End Sub
+
+    Private Sub FitSearch()
+        If pnl_searching.Width > 0 Then
+            pb_searching.Width = pnl_searching.Width / 4
+            pb_searching.Left = pnl_searching.Width / 2 - pb_searching.Width / 2
+            lbl_searching.Left = pnl_searching.Width / 2 - lbl_searching.Width / 2
+        End If
+    End Sub
+
+    Private Sub FitInject()
+        If pnl_injected.Width > 0 Then
+            pb_injecting.Width = pnl_injected.Width / 4
+            pb_injecting.Left = pnl_injected.Width / 2 - pb_injecting.Width / 2
+            lbl_injecting.Left = pnl_injected.Width / 2 - lbl_injecting.Width / 2
+        End If
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        FitSearch()
+        FitInject()
+        pnl_injected.Visible = False
+        pnl_injecting.Visible = False
+        pnl_injected.Location = pnl_injecting.Location
+        pnl_searching.Location = pnl_injected.Location
+
+    End Sub
+
+    Private Sub Update_Injected(ByRef Process As Process)
+        lbl_process_id.Text = "Process ID: " + Process.Id.ToString
+        lbl_process_name.Text = "Process Name: " + Process.ProcessName
+    End Sub
+
+    Private Sub pnl_injecting_SizeChanged(sender As Object, e As EventArgs) Handles pnl_injecting.SizeChanged
+        FitInject()
+    End Sub
+
+    Private Enum Panel
+        Searching
+        Injecting
+        Injected
+    End Enum
+    Private Sub SwitchPanel(ByVal Panel As Panel)
+        Select Case Panel
+            Case Form1.Panel.Searching
+                pnl_searching.Visible = True                
+                pnl_injecting.Visible = False
+                pnl_injected.Visible = False
+                pnl_searching.BringToFront()
+            Case Form1.Panel.Injecting
+                pnl_injecting.Visible = True
+                pnl_searching.Visible = False
+                pnl_injected.Visible = False
+                pnl_injecting.BringToFront()
+            Case Form1.Panel.Injected
+                pnl_injected.Visible = True
+                pnl_searching.Visible = False
+                pnl_injecting.Visible = False
+                pnl_injected.BringToFront()
+        End Select
+    End Sub
+
+    ''Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Long
+    'Private Declare Function FindWindowEx Lib "user32" Alias "FindWindowExA" (ByVal hWnd1 As Long, ByVal hWnd2 As Long, ByVal lpsz1 As String, ByVal lpsz2 As String) As Long
+    'Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hwnd As Long, ByVal wMsg As Long, ByVal wParam As Long, ByVal lParam As String) As Long
+    '<DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    'Private Shared Function PostMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Boolean
+    'End Function
+    'Private Const EM_REPLACESEL = &HC2
+    'Private Const VK_INSERT As Integer = &H2D
+    'Private Const WM_KEYDOWN = &H100
+    'Private Const WM_KEYUP = &H101
+
+    'Private Sub Send()
+
+    '    Dim ret As Long = PostMessage(VermintideProcess.MainWindowHandle, WM_KEYDOWN, VK_INSERT, 0)
+
+
+    '    'Dim nPadHwnd As Long = FindWindow("vermintide", "vermintide")
+    '    'If nPadHwnd > 0 Then
+    '    '    Dim EditHwnd As Long = FindWindowEx(nPadHwnd, 0&, "Edit", vbNullString)
+    '    '    If EditHwnd > 0 Then
+    '    '        'Dim ret As Long = SendMessage(EditHwnd, EM_REPLACESEL, 0, Mid$(SendString, CharPos, 1))
+    '    '        Dim ret As Long = SendMessage(EditHwnd, WM_KEYDOWN, VK_INSERT, 0)
+    '    '        ret = SendMessage(EditHwnd, WM_KEYUP, VK_INSERT, 0)
+    '    '    End If
+    '    'Else
+    '    '    MsgBox("Untitled - Notepad was not found!", vbInformation)
+    '    'End If
+    'End Sub
+
+End Class
